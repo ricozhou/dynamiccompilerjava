2bf0a1f90623a3632321457ccd205efca9026cdb
ricozhou <2320095772@qq.com>
2018-08-23 22:27:31 +0800

    动态编译运行java程序
    动态编译运行java程序

---
+++ dynamiccompilerjava/src/constant/ConfigConstant.java
@@ -0,0 +1,21 @@
+package constant;
+
+/**
+ * 通用配置常量信息
+ * 
+ * @author ricozhou
+ */
+public class ConfigConstant {
+	/**
+	 * 通用java依赖包文件路径常量
+	 */
+	public static final String CONFIG_JAVACSDEPENDENCYLIB = "otherresources\\javacsdependencylib";
+	/**
+	 * 通用默认tools.jar路径
+	 */
+	public static final String CONFIG_DEFAULT_TOOLS_PATH = "lib\\tools.jar";
+	/**
+	 * 通用JAVA_HOME
+	 */
+	public static final String CONFIG_ENV_JAVA_HOME = "JAVA_HOME";
+}
